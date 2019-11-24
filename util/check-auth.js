ecd46cf3bd7a962c8b120e7b74c88086739b8ea9
Hudo Assenco <hudovisk@users.noreply.github.com>
2019-11-24 20:34:22 -0300

    Pequena melhoria na escrita do código
    Apenas uma melhoria pequena na forma como foi escrito o código. Dessa forma você evita muitos blocos aninhados e deixa o código mais legível. Se não me engano é uma das dicas do [Clean Code](https://www.amazon.com.br/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882), recomendo a leitura!

--- util/check-auth.js
+++ util/check-auth.js
@@ -5,19 +5,19 @@ const { SECRET_KEY } = require('../config')
 
 module.exports = (context) => {
     const authHeader = context.req.headers.authorization
-
-    if(authHeader){
-        const token = authHeader.split('Bearer ')[1]
-
-        if(token){
-            try {
-                const user = jwt.verify(token, SECRET_KEY)
-                return user
-            } catch(err){
-                throw new AuthenticationError('Invalid/Expired token')
-            }
-        }
+    if (!authHeader) {
+        throw new Error('Authorization header must be provided')
+    }
+    
+    const token = authHeader.split('Bearer ')[1]
+    if (!token) {
         throw new Error("Authentication token must be 'Bearer [token]'")
     }
-    throw new Error('Authorization header must be provided')
+    
+    try {
+        const user = jwt.verify(token, SECRET_KEY)
+        return user
+    } catch(err){
+        throw new AuthenticationError('Invalid/Expired token')
+    }
 }
